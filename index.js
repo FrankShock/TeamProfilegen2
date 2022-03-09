e8a49ee2c58d6c31859ff29c811d0ebbc08c32cf
FrankShock <schockeyf@gmail.com>
2022-03-08 19:46:36 -0600

    workd setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+const jest = require('jest');
+const fs = require('fs');
+const path = require('path');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const render = require('./src/pageTemp')
+
+const createEmploy = ()  =>  {
+
+    
+}
